a68c0cb223d0084759df0b18f5531e5224e8ffa7
Artak <artak6297@gmail.com>
2021-07-28 08:31:10 +0300

    Created a new Expo app

--- components/NavController.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { useIsLoggedIn } from '../AuthContext'
-
-export default () => {
-  const isLoggedIn = useIsLoggedIn();
-  return <Text>NavController</Text>
-}
