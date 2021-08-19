9ecd2ac2581f534ade42101cbbc9a9d19d5a2d53
Artak <artak6297@gmail.com>
2021-08-19 23:08:32 +0300

    feat(Loader, constants/Layouts): added Loader and constants/Layouts

---
+++ components/Loader.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import styled from 'styled-components/native';
+
+import { Layout } from '../constants/Layout';
+import { theme } from '../styles';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  height: ${Layout.window.height / 2}px;
+`;
+
+export const Loader = () => {
+  return (
+    <Container>
+      <ActivityIndicator color={theme.darkGreyColor} />
+    </Container>
+  );
+};
