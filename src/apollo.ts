709f5569b3b7804a7f9a9ac481aad72bdb452065
Artak <artak6297@gmail.com>
2021-08-24 08:30:01 +0300

    feat(apollo): added interceptor

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,4 +1,36 @@
-export const apolloClientOptions = {
-    uri: "http://localhost:4000",
-}
+import { from, HttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000",
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await AsyncStorage.getItem("jwt");
+  console.log("--> request update -->", token);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const link = from([errorLink, httpLink]);
+
+export const apolloClientOptions = {
+  link: authLink.concat(link),
+};
