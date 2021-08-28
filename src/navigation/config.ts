5fc4829c116c9bb1fe263bacac06bc72503b322a
Artak <artak6297@gmail.com>
2021-08-28 18:23:41 +0300

    refactor(navigation): changed the names of the routes

--- src/navigation/config.ts
+++ src/navigation/config.ts
@@ -2,38 +2,46 @@ export const stackStyles = {
   backgroundColor: "#FAFAFA",
 };
 
+export type BottomTabStackParamList = {
+  Home: undefined;
+  Search: undefined;
+  Add: undefined;
+  Notification: undefined;
+  Profile: undefined;
+};
+
 export enum BottomTabNavigationRoutes {
-  HOME = "HOME",
-  SEARCH = "SEARCH",
-  ADD = "ADD",
-  NOTIFICATION = "NOTIFICATION",
-  PROFILE = "PROFILE",
+  HOME = "Home",
+  SEARCH = "Search",
+  ADD = "Add",
+  NOTIFICATION = "Notification",
+  PROFILE = "Profile",
 }
 
 export enum MainNavigationRoutes {
-  BOTTOM_TABS = "BOTTOM_TABS",
-  PHOTO = "PHOTO",
-  MESSAGES_PAGE = "MESSAGES_PAGE",
+  BOTTOM_TABS = "Bottom_tabs",
+  PHOTO = "Photo",
+  MESSAGES_PAGE = "Messages_page",
 }
 
 export enum AuthNavigationRoutes {
-  HOME = "HOME",
-  SIGNUP = "SIGNUP",
-  LOGIN = "LOGIN",
-  CONFIRM = "CONFIRM",
+  HOME = "Home",
+  SIGNUP = "Signup",
+  LOGIN = "Login",
+  CONFIRM = "Confirm",
 }
 
 export enum MessageNavigationRoutes {
-  MESSAGE = "MESSAGE",
-  MESSAGES = "MESSAGES",
+  MESSAGE = "Message",
+  MESSAGES = "Messages",
 }
 
 export enum PhotoNavigationRoutes {
-  PHOTO_TAB = "PHOTO_TAB",
-  UPLOAD_PHOTO = "UPLOAD_PHOTO",
+  PHOTO_TAB = "Photo_tab",
+  UPLOAD_PHOTO = "Upload_photo",
 }
 
 export enum PhotoTabNavigationRoutes {
-  SELECT_PHOTO = "SELECT_PHOTO",
-  TAKE_PHOTO = "TAKE_PHOTO",
+  SELECT_PHOTO = "Select_photo",
+  TAKE_PHOTO = "Take_photo",
 }
